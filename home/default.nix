6cc3b4a1931ae7cd0efe70261863dbc7a3fae4e9
Nguyen Dinh Vu <nguyendinhvu@msn.com>
2025-09-06 22:44:52 +0000

    feat:remove git-credential-manager

--- home/default.nix
+++ home/default.nix
@@ -12,7 +12,6 @@
     neovim
     wget
     curl
-    git-credential-manager
   ];
 
   # Set your shell to zsh (or bash, fish, etc.)
@@ -22,14 +21,6 @@
     enable = true;
     userName = "Nguyen Dinh Vu";
     userEmail = "nguyendinhvu@msn.com";
-    # Tell Git to use the credential manager
-    extraConfig = {
-      credential = {
-        # This tells the Git Credential Manager to use a cache.
-        credentialStore = "cache";
-        helper = "${pkgs.git-credential-manager}/bin/git-credential-manager";
-      };
-    };
   };
 
   # This is the version of Home Manager you are targeting.
