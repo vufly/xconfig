ee4b8a5ef1ea2e41f1387c6a3aba35bf047a0d6f
Nguyen Dinh Vu <nguyendinhvu@msn.com>
2025-09-06 22:21:44 +0000

    feat:add git-credential-manager

--- home/default.nix
+++ home/default.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  # Set your username and home directory
+  home.username = "vudinhn";
+  home.homeDirectory = "/home/vudinhn";
   # Install some core packages to get you started
   home.packages = with pkgs; [
     git
@@ -9,8 +12,25 @@
     neovim
     wget
     curl
-    # Add more packages here
+    git-credential-manager
   ];
+
+  # Set your shell to zsh (or bash, fish, etc.)
+  programs.zsh.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Nguyen Dinh Vu";
+    userEmail = "nguyendinhvu@msn.com";
+    # Tell Git to use the credential manager
+    extraConfig = {
+      credential = {
+        # This tells the Git Credential Manager to use a cache.
+        credentialStore = "cache";
+        helper = "${pkgs.git-credential-manager}/bin/git-credential-manager";
+      };
+    };
+  };
 
   # This is the version of Home Manager you are targeting.
   # This helps manage breaking changes.
