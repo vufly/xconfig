a80200362eb2e9d61f43f8a1f19309cd2dc17f6d
Nguyen Dinh Vu <nguyendinhvu@msn.com>
2025-09-07 08:40:09 +0000

    feat:clean the config

--- home/default.nix
+++ home/default.nix
@@ -18,18 +18,5 @@
     userEmail = "nguyendinhvu@msn.com";
   };
 
-  programs.zsh = {
-    enable = true;
-    oh-my-zsh.enable = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-  };
-
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-  };
-
   home.stateVersion = "22.11";
 }
